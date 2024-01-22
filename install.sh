651a19d75e57cdc5a5a96e761fea91085a0553b4
arrox <georg@arrox.at>
2024-01-22 20:47:27 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+apt install weston chromium
+useradd -m kiosk
+mkdir /home/kiosk/.config
+cp weston.ini /home/kiosk/.config
+cp kiosk.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable kiosk.service
+cp 61-evdev-local.hwdb /etc/udev/hwdb.d
+cp 99-touch-mirror.rules /etc/udev/rules.d
+ln -sf /dev/null /etc/udev/rules.d/90-libinput-fuzz-override.rules
+systemd-hwdb update
+systemctl disable getty@.service
+
+echo "NAutoVTs=0" > /etc/systemd/logind.conf
+echo "ReserveVT=0" > /etc/systemd/logind.conf
+echo " quiet nosplash loglevel=0 vt.global_cursor_default=0" > /boot/cmdline.txt
+echo " avoid_warnings=1" > /boot/config.txt
+echo " disable_splash=1" > /boot/config.txt
+
+systemctl disable keyboard-setup.service
+systemctl disable dphys-swapfile.service
+systemctl disable avahi-daemon.service
+systemctl disable sys-kernel-debug.mount
+systemctl disable raspi-config.service
+systemctl disable systemd-udev-trigger.service
+systemctl disable rpi-eeprom-update.service
+systemctl disable rsyslog.service
+systemctl disable systemd-journald.service
+systemctl disable systemd-fsck-root.service
+systemctl disable systemd-logind.service
+systemctl disable bluetooth.service
+systemctl disable hciuart.service
+
+reboot
