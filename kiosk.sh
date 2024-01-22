c47279b164bb7ff0a2e57bf71dc24d5d188918c9
arrox <georg@arrox.at>
2024-01-22 22:16:16 +0100

    fix stupid

--- kiosk.sh
+++ kiosk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chromium-browser homeassistant.realraum.at --touch-noise-filtering --start-fullscreen --window-size=1280,1024 --window-position=0,0 --kiosk --noerrdialogs --disable-infobars --disable-translate --no-first-run --fast --fast-start --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
+chromium homeassistant.realraum.at --touch-noise-filtering --start-fullscreen --window-size=1280,1024 --window-position=0,0 --kiosk --noerrdialogs --disable-infobars --disable-translate --no-first-run --fast --fast-start --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
