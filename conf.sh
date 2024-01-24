c504252f169b6c13031559788c15bf3ff2b3dd33
arrox <georg@arrox.at>
2024-01-24 06:31:42 +0100

    completely change everything

---
+++ conf.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+read -p "Enter username: " username
+read -s -p "Enter password: " password
+echo
+read -p "Enter your WiFi SSID: " wifi_ssid
+read -s -p "Enter your WiFi password: " wifi_password
+echo
+read -p "Enter hostname: " hostname
+
+if [ -z "$username" ] || [ -z "$password" ] || [ -z "$wifi_ssid" ] || [ -z "$wifi_password" ] || [ -z "$hostname" ]; then
+    echo "Invalid input. Exiting script."
+    exit 1
+fi
+
+cat <<EOF > firstboot.sh
+
+echo "$hostname" > /etc/hostname
+systemctl enable ssh
+
+DEF_USER=$(awk -F':' -v uid=1000 '$3 == uid { print $1 }' /etc/passwd)
+uderdel -r $DEF_USER
+
+useradd -m $username
+echo '$username:$password' | chpasswd
+usermod -aG sudo $username
+
+/kioskinstall/install.sh
+reboot
+
+EOF
+
+cat <<EOF > 20-wireless.network
+[Match]
+Name=wlan0
+
+[Network]
+DHCP=yes
+
+[Wireless]
+SSID=$wifi_ssid
+KeyManagement=WPA-PSK
+PSK=$wifi_password
+
+EOF
