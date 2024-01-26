0e96dde49bf43d3924b133841b3283a74807646b
arrox <georg@arrox.at>
2024-01-26 03:23:50 +0100

    why?

--- install.sh
+++ install.sh
@@ -14,6 +14,18 @@ wget -nv --show-progress -O - $DEBIAN_DL_LINK | unxz > debian.img
 
 LOOP_DEVICE=$(losetup -fP --show debian.img)
 
+mkdir boot
+mount "${LOOP_DEVICE}p1" ./boot
+
+echo "avoid_warnings=1" >> ./boot/config.txt
+echo "disable_splash=1" >> ./boot/config.txt
+
+sed -i 's/$/ \*quiet nosplash loglevel=0 vt.global_cursor_default=0\*/' ./boot/cmdline.txt
+
+sync
+umount ./boot
+rmdir ./boot
+
 mkdir image
 mount "${LOOP_DEVICE}p2" ./image
 
