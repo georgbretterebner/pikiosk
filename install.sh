bbb8107766b0a3fe5fe93f43e6d9c7dea47655c1
arrox <georg@arrox.at>
2024-01-26 01:21:13 +0100

    rm cef stuff

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# To get the newest download link for cef please visit and copy
-#   the link from: Current Stable Build - Sample Application
-#
-# https://cef-builds.spotifycdn.com/index.html#linuxarm64
-#
-# For debian please go to and copy the desired Link
+# For latest debian download please go to
 # https://raspi.debian.net/daily-images/
-
-CEF_DL_LINK="https://cef-builds.spotifycdn.com/cef_binary_120.2.7%2Bg4bc6a59%2Bchromium-120.0.6099.234_linuxarm64_client.tar.bz2"
+# and copy the desired Link.
 DEBIAN_DL_LINK="https://raspi.debian.net/daily/raspi_3_bookworm.img.xz"
 
 
@@ -21,22 +15,12 @@ fi
 
 wget -O - $DEBIAN_DL_LINK | unxz > debian.img
 
-dd if=/dev/zero bs=10M count=200 >> debian.img
 LOOP_DEVICE=$(losetup -fP --show debian.img)
-parted "$LOOP_DEVICE" resizepart 2 4G
-resize2fs "${LOOP_DEVICE}p2"
 
 mkdir image
 mount "${LOOP_DEVICE}p2" ./image
 
-rm -rf ./cef-bin
-mkdir cef-bin
-wget -O - $CEF_DL_LINK |
-tar -xj --strip-components=2 -C cef-bin
-
 mkdir ./image/kiosksetup
-mkdir ./image/kiosksetup/cef-bin
-mv cef-bin ./image/kiosksetup
 cp -r ./files/* ./image/kiosksetup
 chown -R root ./image/kiosksetup
 
